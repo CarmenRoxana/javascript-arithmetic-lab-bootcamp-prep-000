744ea94b71292699b7fcc03b7254b5d7343d9100
Roxana <roxanafetea@yahoo.com>
2018-03-30 05:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function inc(n) {
   return ++n;
 }
 
-function makeInt(n){
-  return parseInt('n', 10)
+function makeInt(n) {
+parseInt('n', 10);
 }
 
-function preserveDecimal(n){
-   return parseFloat('n')
+function preserveDecimal(n) {
+   parseFloat('n');
  
 }
